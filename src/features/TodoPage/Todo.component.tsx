6227acd56f9df25240ed623da98282053911e8d7
Dmytro <dimakhved@gmail.com>
2024-02-13 10:59:50 +0200

    fixed: modal window, add: TodoCard

--- src/features/TodoPage/Todo.component.tsx
+++ src/features/TodoPage/Todo.component.tsx
@@ -1,61 +1,107 @@
-import {Button} from "@mui/material";
-import {useEffect, useState} from "react";
-import {Todo} from "../../models/todos/Todo.model";
+import { Button, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Todo } from "../../models/todos/Todo.model";
 import TodoApi from "../../app/api/todo/Todo.api";
-import "./Todo.component.css"
+import "./Todo.component.css";
 import ModalForm from "./components/ModalForm";
 
-
 const TodoComponent = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
 
-    const [todos, setTodos] = useState<Todo[]>([]);
-    const [modalOpen, setModalOpen] = useState<boolean>(false);
-
-    const fetchTodos = async () => {
-        try {
-            const fetchTodos = await TodoApi.getAll();
-            setTodos(fetchTodos);
-        } catch (error) {
-            console.log("Failed to fetch todos: ", error);
-        }
+  const getPriorityColor = (priority: string) => {
+    switch (priority) {
+      case "high":
+        return "red";
+      case "medium":
+        return "orange";
+      case "low":
+        return "green";
     }
+  };
 
-    useEffect(() => {
-        fetchTodos();
-    }, [])
-
-    const deleteTodo = async (id: number) => {
-        try {
-            await TodoApi.deleteTodo(id);
-            fetchTodos();
-        } catch (error){
-            console.log("Failed to delete todo: ", error)
-        }
+  const fetchTodos = async () => {
+    try {
+      const fetchTodos = await TodoApi.getAll();
+      setTodos(fetchTodos);
+    } catch (error) {
+      console.log("Failed to fetch todos: ", error);
     }
+  };
 
-    return (
-        <>
-            {modalOpen && (
-                <div className="modalBackdrop">
-                    <ModalForm setModalOpen={setModalOpen}></ModalForm>
+  const dateParser = (date: string) => {
+    const updatedDate = date.split("T");
+    return updatedDate[0];
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const deleteTodo = async (id: number) => {
+    try {
+      await TodoApi.deleteTodo(id);
+      fetchTodos();
+    } catch (error) {
+      console.log("Failed to delete todo: ", error);
+    }
+  };
+
+  const handleTodoCreated = () => {
+    fetchTodos();
+  };
+
+  return (
+    <>
+      {modalOpen && (
+        <div className="modalBackdrop">
+          <ModalForm
+            setModalOpen={setModalOpen}
+            onTodoCreated={handleTodoCreated}
+          ></ModalForm>
+        </div>
+      )}
+      <div className={`${modalOpen ? "contentDimmed" : ""}`}>
+        <div className="navbar">
+          <Button onClick={() => setModalOpen(true)}>Create Todo</Button>
+        </div>
+        <div className="todoWrapper">
+          <div className="left">
+            <div className="todoCard">
+              {todos.map((todo) => (
+                <div key={todo.id} className="todoWrap">
+                  <div>
+                    <Typography sx={{ fontWeight: 800, fontSize: 20 }}>
+                      {todo.title}
+                    </Typography>
+                    <Typography className="todoDescription">
+                      {todo.description}
+                    </Typography>
+                    <Typography>
+                      {dateParser(todo.startDate)} - {dateParser(todo.endDate)}
+                    </Typography>
+                    <Typography>Priority: <span style={{ color: getPriorityColor(todo.priority) }}>{todo.priority}</span> </Typography>
+                      
+                  </div>
+                  <div className="todoCard__buttons">
+                    <Button variant="outlined">View</Button>
+                    <Button
+                      variant="outlined"
+                      onClick={() => deleteTodo(todo.id)}
+                      color="error"
+                    >
+                      Delete
+                    </Button>
+                  </div>
                 </div>
-            )}
-            <div className={modalOpen ? 'contentDimmed' : ''}>
-                {todos.map((todo) => (
-                    <div key={todo.id} className="todoWrap">
-                        {todo.title}
-                        <Button onClick={() => deleteTodo(todo.id)}>
-                            Delete
-                        </Button>
-                    </div>
-                ))}
-                <Button onClick={() => setModalOpen(true)}>
-                    Create Todo
-                </Button>
+              ))}
             </div>
-        </>
-    )
-}
-
+          </div>
+          <div className="right">block</div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default TodoComponent;
--- src/features/TodoPage/components/ModalForm.tsx
+++ src/features/TodoPage/components/ModalForm.tsx
@@ -1,77 +1,155 @@
-import {useForm, SubmitHandler} from "react-hook-form"
-import {Todo} from "../../../models/todos/Todo.model";
-import Input from '@mui/joy/Input';
-import {FormControl, InputLabel, Select, MenuItem, Button, TextField, FormLabel} from "@mui/material";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useEffect, useState } from "react";
+import { Todo } from "../../../models/todos/Todo.model";
+import Input from "@mui/joy/Input";
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+  TextField,
+  FormLabel,
+} from "@mui/material";
 import TodoApi from "../../../app/api/todo/Todo.api";
 import React from "react";
 
 interface ModalFormProps {
-    setModalOpen: (isOpen: boolean) => void; // Correctly type the setModalOpen function
+  setModalOpen: (isOpen: boolean) => void;
+  onTodoCreated: () => void;
 }
 
-const ModalForm: React.FC<ModalFormProps> = ({setModalOpen}) => {
-    const {
-        register,
-        handleSubmit,
-        formState: {errors},
-        reset
-    } = useForm<Todo>()
-    const onSubmit: SubmitHandler<Todo> = async (data) => {
-        try {
-            await TodoApi.create(data);
-            reset();
-            setModalOpen(false);
-        } catch (error) {
-            console.log(error);
-        }
+const ModalForm: React.FC<ModalFormProps> = ({
+  setModalOpen,
+  onTodoCreated,
+}) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<Todo>();
+
+  const today = new Date().toISOString().split("T")[0];
+
+  const formatDate = (date: Date) => {
+    return date.toISOString().split("T")[0];
+  };
+
+  const [taskData, setTaskData] = useState({
+    startDateTime: formatDate(new Date()), // Today's date as the default start date
+    dueDateTime: "", // Empty initially
+  });
+
+  useEffect(() => {
+    // Ensure the end date is not before the start date
+    if (taskData.dueDateTime && taskData.startDateTime > taskData.dueDateTime) {
+      setTaskData({ ...taskData, dueDateTime: taskData.startDateTime });
     }
+  }, [taskData.startDateTime]);
 
-    return (
-        <div className="modalWindow">
-            <form onSubmit={handleSubmit(onSubmit)} className="modalWindow__form">
-                <TextField fullWidth placeholder="Title" {...register("title", {required: true})}/>
-                <TextField fullWidth multiline rows={3}
-                           placeholder="Description" {...register("description", {required: true})}/>
-                <FormControl fullWidth>
-                    <InputLabel id="priority-label">Priority</InputLabel>
-                    <Select
-                        labelId="priority-label"
-                        label="Priority"
-                        {...register("priority", {required: true})}
-                    >
-                        <MenuItem value="high">High</MenuItem>
-                        <MenuItem value="medium">Medium</MenuItem>
-                        <MenuItem value="low">Low</MenuItem>
-                    </Select>
-                </FormControl>
-                <div className="modalWindow__date">
-                    <div className="modalWindow__date__inner">
-                        <FormLabel>Start Date</FormLabel>
-                        <Input
-                               placeholder="Start Date"
-                               type="date"
-                               sx={{padding: 1.5}}
-                        />
-                    </div>
-                    <div className="modalWindow__date__inner">
-                        <FormLabel>End Date</FormLabel>
-                        <Input
-                               type="date"
-                               sx={{padding: 1.5}}
-                        />
-                    </div>
-                </div>
-                <div className="modalWindow__buttons">
-                    <Button type="submit" variant="contained">Create</Button>
-                    <Button variant="contained" color="error" onClick={() => {
-                        setModalOpen(false)
-                    }}>Cancel</Button>
-                </div>
-            </form>
+  // Handler to update state on input change
+  const handleChange = (value: string, fieldName: string) => {
+    setTaskData({
+      ...taskData,
+      [fieldName]: value,
+    });
+  };
 
+  const onSubmit: SubmitHandler<Todo> = async (data) => {
+    try {
+      const isCreated = await TodoApi.create(data);
+      reset();
+      setModalOpen(false);
+      if (isCreated) {
+        onTodoCreated();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  return (
+    <div className="modalWindow">
+      <form onSubmit={handleSubmit(onSubmit)} className="modalWindow__form">
+        <TextField
+          fullWidth
+          placeholder="Title"
+          {...register("title", { required: true })}
+        />
+        <TextField
+          fullWidth
+          multiline
+          rows={3}
+          placeholder="Description"
+          {...register("description", { required: true })}
+        />
+        <FormControl fullWidth>
+          <InputLabel id="priority-label">Priority</InputLabel>
+          <Select
+            labelId="priority-label"
+            label="Priority"
+            {...register("priority", { required: true })}
+          >
+            <MenuItem value="high">High</MenuItem>
+            <MenuItem value="medium">Medium</MenuItem>
+            <MenuItem value="low">Low</MenuItem>
+          </Select>
+        </FormControl>
+        <div className="modalWindow__date">
+          <div className="modalWindow__date__inner">
+            <FormLabel>Start Date</FormLabel>
+            <TextField
+              {...register("startDate", { required: true })}
+              placeholder="Start Date"
+              type="date"
+              sx={{ display:"flex" }}
+              value={taskData.startDateTime}
+              onChange={(e) => handleChange(e.target.value, "startDateTime")}
+              InputProps={{
+                inputProps: {
+                  min: today,
+                  max: taskData.dueDateTime,
+                },
+              }}
+            />
+          </div>
+          <div className="modalWindow__date__inner">
+            <FormLabel>End Date</FormLabel>
+            <TextField
+              {...register("endDate", { required: true })}
+              type="date"
+              sx={{ display:"flex" }}
+              value={taskData.dueDateTime}
+              onChange={(e) => handleChange(e.target.value, "dueDateTime")}
+              InputProps={{
+                inputProps: {
+                  min:
+                    taskData.startDateTime === ""
+                      ? today
+                      : taskData.startDateTime,
+                },
+              }}
+            />
+          </div>
         </div>
-    )
-}
+        <div className="modalWindow__buttons">
+          <Button type="submit" variant="contained">
+            Create
+          </Button>
+          <Button
+            variant="contained"
+            color="error"
+            onClick={() => {
+              setModalOpen(false);
+            }}
+          >
+            Cancel
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default ModalForm;
